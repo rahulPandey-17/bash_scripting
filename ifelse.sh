79f585ef3a11f64a439feb5a41746615ca61ee2c
Rahul Pandey <0517rp.rahul@gmail.com>
2024-09-23 17:17:26 +0530

    ifelse

--- ifelse.sh
+++ ifelse.sh
@@ -2,11 +2,20 @@
 
 # we can use if-else statements in bash scripting
 
-name="Rahul"
+num=200
 
-if [ $name = "Rahul" ]
+if [ $num -ne 100 ]
 then
     echo "Condition is right"
 else
     echo "Condition is wrong"
+fi
+
+# we can also check if a file is present in the system or not using if-else
+
+if [ -f ~/learning/bash_scripting/firstCommand.sh ]
+then
+    echo "File exists"
+else
+    echo "File does not exist"
 fi
